010f4127c0b7d5c1591d0ee43fb601af7a92a72d
PatoBeltran <pbelsal@gmail.com>
2015-05-05 15:10:58 -0500

    Add Juzgados Scrapper for Baja California sur

--- estados/BajaCaliforniaSur/juzgados.rb
+++ estados/BajaCaliforniaSur/juzgados.rb
@@ -15,10 +15,27 @@ module Topolegal
         @endpoint = "http://www.tribunalbcs.gob.mx/listas.php"
       end
 
-      def run
-        page = Mechanize.new(@enpoint)
+      def parse_juzgados(html)
+        doc = Nokogiri::HTML(html)
 
-        err
+        spans = doc.xpath("//tr/td[position()>last()-2]/span[position()>1]")
+
+        juzgados = []
+        name_to_append = ""
+        spans.each do |span|
+          if span.xpath("@id='subtitulos'")
+            name_to_append = span.text
+          else
+            juzgados << "#{span.xpath("a").first.text}, #{name_to_append}"
+          end
+        end
+        @results = juzgados
+      end
+
+      def run
+        page = Mechanize.new.get(@endpoint)
+
+        parse_juzgados(page.body)
       end
     end
   end
